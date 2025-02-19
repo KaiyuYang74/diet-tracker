b1763cc07f3f2a96b4e9358932f3e5a81e41afd1
Kaiyu Yang <k276yang@uwaterloo.ca>
2025-02-19 02:54:46 -0500

    merge auth functionalities

--- src/main/java/com/example/diettracker/DietTrackerApplication.java
+++ src/main/java/com/example/diettracker/DietTrackerApplication.java
@@ -2,15 +2,10 @@ package com.example.diettracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 
-@ServletComponentScan
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class DietTrackerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DietTrackerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DietTrackerApplication.class, args);
+    }
 }
